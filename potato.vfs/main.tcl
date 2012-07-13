697c85b026d353c1f0e26a86bd58a6dedfa7189a
talvo <talvo@talvo.com>
2012-07-13 23:40:17 +0000

    Hopefully allow better running of main.tcl on Linux from source from other dirs.

--- potato.vfs/main.tcl
+++ potato.vfs/main.tcl
@@ -1,7 +1,8 @@
 #!/usr/bin/tclsh
 
+  cd [file dirname [info script]]
   if { [catch {package require starkit}] } {
-       lappend auto_path [file join [file dirname [info script]]] lib app-potato]
+       lappend auto_path [file normalize [file join [file dirname [info script]]] lib app-potato]]
      } else {
        starkit::startup
      }
