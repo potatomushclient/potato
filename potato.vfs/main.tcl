d4282602b5ed61d5b592fd196f8da63f857263bb
aiviru <aiviru@2f0d5682-6a73-11de-8235-5b595ee06194>
2012-07-14 00:22:28 +0000

    Bracket mismatch.

--- potato.vfs/main.tcl
+++ potato.vfs/main.tcl
@@ -1,7 +1,7 @@
 #!/usr/bin/tclsh
 
   if { [catch {package require starkit}] } {
-       lappend auto_path [file normalize [file join [file dirname [info script]] lib app-potato]]]
+       lappend auto_path [file normalize [file join [file dirname [info script]] lib app-potato]]
      } else {
        starkit::startup
      }
