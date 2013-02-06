558bc22af023c86569d1cbc2e0f30d2130c36731
aiviru <aiviru@2f0d5682-6a73-11de-8235-5b595ee06194>
2013-02-06 02:30:08 +0000

    Add treeviewUtils path to main.tcl
    This stops things from breaking in a Linux/from source situation for me.

--- trunk/potato.vfs/main.tcl
+++ trunk/potato.vfs/main.tcl
@@ -2,6 +2,7 @@
 
   if { [catch {package require starkit}] } {
        lappend auto_path [file normalize [file join [file dirname [info script]] lib app-potato]]
+      lappend auto_path [file normalize [file join [file dirname [info script]] lib treeviewUtils]]
      } else {
        starkit::startup
      }
